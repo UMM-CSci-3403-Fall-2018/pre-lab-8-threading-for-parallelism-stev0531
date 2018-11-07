29064cd13e67a56530a89cc2738b28c339c5c493
stev0531 <stev0531@morris.umn.edu>
2018-11-07 17:46:06 -0600

    Fix index error exception

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -29,36 +29,18 @@ public class ThreadedSearch<T> implements Searcher<T>, Runnable {
      * You can assume that the list size is divisible by `numThreads`
      */
     public boolean search(T target, List<T> list) throws InterruptedException {
-        /*
-         * First construct an instance of the `Answer` inner class. This will
-         * be how the threads you're about to create will "communicate". They
-         * will all have access to this one shared instance of `Answer`, where
-         * they can update the `answer` field inside that instance.
-         *
-         * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-         * using the 5 argument constructor for the class. You'll hand each of
-         * them the same `target`, `list`, and `answer`. What will be different
-         * about each instance is their `begin` and `end` values, which you'll
-         * use to give each instance the "job" of searching a different segment
-         * of the list. If, for example, the list has length 100 and you have
-         * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-         * [50, 75), and [75, 100) as their sections to search.
-         *
-         * You then construct `numThreads`, each of which is given a different
-         * instance of this class as its `Runnable`. Then start each of those
-         * threads, wait for them to all terminate, and then return the answer
-         * in the shared `Answer` instance.
-         */
        try {
          Answer answer = new Answer();
          int searchIncrement = list.size()/numThreads;
 
+         // Creates `numThreads` of Threads with their section to search on the list
          Thread threads[] = new Thread[numThreads];
          for(int i=0; i<numThreads; i++) {
            threads[i] = new Thread(new ThreadedSearch<T>(target, list, i*searchIncrement, (i+1)*searchIncrement, answer));
            threads[i].start();
           }
 
+          // Waits for threads to finish
           for (int i=0; i<numThreads; ++i) {
            threads[i].join();
           }
@@ -73,9 +55,14 @@ public class ThreadedSearch<T> implements Searcher<T>, Runnable {
     }
 
     public void run() {
-        for(int j = begin; this.begin < this.end; j++) {
+        for(int j = begin; j < end; j++) {
           if(target.equals(list.get(j))) {
             answer.setAnswer(true);
+          }
+
+          // Breaks for loop if a thread has found the answer
+          if(answer.getAnswer() == true) {
+            break;
           }
         }
     }
